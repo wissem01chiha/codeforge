4d86871eb10d8bd5c8d6a08b437ca5a112a4e446
wissem01chiha <chihawissem08@gmail.com>
2025-03-30 21:59:06 +0100

    setting .travis.yml file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+pub trait FunParser{
+
+}
+pub trait ClassParser {
+
+}
+
+
+// basic genrator for language specific routines (functions , classes )
+pub trait TestGenerator {
+    fn generate(&self, function_name: &str) -> String;
+}
+
+// Factory Function
+pub fn create_generator(language: &str) -> Box<dyn TestGenerator> {
+    match language {
+        "C++" => Box::new(GoogleTestGenerator),
+        "Python" => Box::new(PytestGenerator),
+        _ => panic!("Unsupported language!"),
     }
+}
+
+
+// google test plugin gnerator 
+// by defut it gnerate all possible template tests and 
+// corrponding documenttaion of each test case
+pub struct GoogleTestGenerator;
+
+impl TestGenerator for GoogleTestGenerator {
+    fn generate(&self, function_name: &str) -> String {
+        format!("TEST({}, {}) {{ /* TODO */ }}", function_name, "TestCaseName")
+    }
+}
+
+// basic types can be passed to a function, declraitves, 
+enum fun_t {
+    bool,
+    int32,
+    int64,
+    int8,
+    float
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
+
+mod lib;
+
 fn main() {
     println!("Hello, world!");
 }
 
 
-pub trait TestGenerator {
-    fn generate(&self, function_name: &str) -> String;
-}
 
-pub struct GoogleTestGenerator;
-impl TestGenerator for GoogleTestGenerator {
-    fn generate(&self, function_name: &str) -> String {
-        format!("TEST({}, {}) {{ /* TODO */ }}", function_name, "TestCaseName")
-    }
-}
+
 
 pub struct PytestGenerator;
 impl TestGenerator for PytestGenerator {
@@ -21,11 +16,4 @@ impl TestGenerator for PytestGenerator {
     }
 }
 
-// Factory Function
-pub fn create_generator(language: &str) -> Box<dyn TestGenerator> {
-    match language {
-        "C++" => Box::new(GoogleTestGenerator),
-        "Python" => Box::new(PytestGenerator),
-        _ => panic!("Unsupported language!"),
-    }
-}
+
